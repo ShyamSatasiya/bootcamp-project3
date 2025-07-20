934f0e166688a55495574935ac7e70e748837f1c
Shyam Satasiya <shyamsatasiya10@gmail.com>
2025-07-20 17:18:18 -0400

    Update policy definition ID for location restriction in policy.tf

--- policy.tf
+++ policy.tf
@@ -2,7 +2,8 @@
 resource "azurerm_resource_group_policy_assignment" "location_restriction" {
   name                = "restrict-locations"
   resource_group_id     = azurerm_resource_group.rg.id
-  policy_definition_id = data.azurerm_policy_definition.allowed_locations.id
+  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/AllowedLocations"
+
 
   parameters = <<PARAMS
     {
